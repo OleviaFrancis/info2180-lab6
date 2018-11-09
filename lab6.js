3416807dc7246e4470834d44345dcf86f17f20d1
Olevia Francis <olevia.francis@mymona.uwi.edu>
2018-11-09 17:59:38 +0000

    Initial file

---
+++ lab6.js
@@ -0,0 +1,7 @@
+wondow.onload = function() {
+    var searchbutton = document.getElementById("searchbutton");
+    searchbutton.onclick = function() {
+        console.log('Button Clicked')
+    };
+    //console.log(searchbutton)
+};
