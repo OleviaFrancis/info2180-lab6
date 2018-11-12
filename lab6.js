570f520cdfc359e3187bd9de153b305f76c15ccf
Olevia Francis <olevia.francis@mymona.uwi.edu>
2018-11-12 20:16:38 +0000

    Exercise 2

--- lab6.js
+++ lab6.js
@@ -1,7 +1,29 @@
-wondow.onload = function() {
+window.onload = function() {
+    var text = document.getElementById(text);
     var searchbutton = document.getElementById("searchbutton");
     searchbutton.onclick = function() {
         console.log('Button Clicked')
+        
+    var httpRequest = new XMLHttpRequest();
+    var url = "request.php?q=definition";
+    httpRequest.onreadystatechange = function(){
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+         if (httpRequest.status === 200) {
+            var response = httpRequest.responseText;
+            alert(response);
+            } else {
+            alert('There was a problem with the request.');
+            }
+        } 
     };
-    //console.log(searchbutton)
+            
+    httpRequest.open('GET', url);
+    httpRequest.send();
+    
+    };
+    console.log(searchbutton)
+    
+    
 };
+
+       
