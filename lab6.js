45387672d9d6945327974c6dacb33bd2498c0cbe
Olevia Francis <olevia.francis@mymona.uwi.edu>
2018-11-13 21:40:18 +0000

    Exercise 4 ish

--- lab6.js
+++ lab6.js
@@ -2,7 +2,7 @@ window.onload = function() {
     var text = document.getElementById(text);
     var searchbutton = document.getElementById("searchbutton");
     searchbutton.onclick = function() {
-        console.log('Search button Clicked')
+        console.log('Search button Clicked');
         var httpRequest = new XMLHttpRequest();
         var r  = document.getElementById("textBox");
         var url = "request.php?q="+ r.value;
@@ -22,11 +22,27 @@ window.onload = function() {
         httpRequest.send();
     };
     
-    console.log(searchbutton)
+    console.log(searchbutton);
     
+    var defbutton = document.getElementById("defbutton");
     defbutton.onclick = function() {
-        console.log('Get Definition button clicked')
-        
+        console.log('Get definition button clicked');
+        var httpRequest = new XMLHttpRequest();
+        var url = "request.php?q=&all=true";
+        httpRequest.onreadystatechange = function(){
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+         if (httpRequest.status === 200) {
+            var response = httpRequest.responseXML;
+                document.getElementById("Result").innerHTML=response;
+           
+            } else {
+            alert('There was a problem with the request.');
+            }
+        } 
+        };
+            
+        httpRequest.open('GET', url);
+        httpRequest.send();
     };
     
 };
--- request.php
+++ request.php
@@ -4,15 +4,35 @@
 // respond with a value
 
 $query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
+if ($_GET["all"]=="true"){
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<definitions>
+    <definition name = "definition">
+        A statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    <definition name = "bar">
+        A place that sells alcholic beverages.
+    </definition>
+    <definition name = "ajax">
+        <p>Technique which involves the use of javascript and xml (or JSON).</p>
+    </definition>
+    <definition name = "html">
+        The standard markup language for creating web pages and web applications.
+    </definition>
+    <definition name= "css">
+        A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    <definition name = "javascript">
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    <definition name = "php">
+        A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+    </definition>
+</definitions>';
 
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();
+}else{
 print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+print "<p>" . $definition[$query] . "</p>";}
